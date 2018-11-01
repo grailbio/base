c3cd18d7b1e90991141b84f9b4e71d2108ce1652
Swami Mahadevan <smahadevan@grailbio.com>
2018-11-01 00:56:36 -0700

    Add an admission controller
    Summary:
    Add an admission controller under base/admit and some unit tests.
    
    The controller works in the following manner:
    - starts off with a max number of tokens and and grants upto 10% more tokens upon Acquire()
    - increases max if no capacity errors were seen at a higher usage upon Release().
    - decreases max by 10% if capacity errors were seen.
    
    (Use of this in s3blob will be in a follow-up revision).
    
    Test Plan:
    Unit tests have been added.
    
    Additionally, I will experiment with real concurrent S3 operations to figure out reasonably optimal parameters.
    
    Reviewers: marius, pgopal
    
    Reviewed By: marius
    
    Subscribers: marius
    
    Differential Revision: https://phabricator.grailbio.com/D20691
    
    fbshipit-source-id: e5c1308

---
+++ admit/admit.go
@@ -0,0 +1,148 @@
+// Copyright 2018 GRAIL, Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Package admit contains utilities for admission control.
+package admit
+
+import (
+	"context"
+	"errors"
+	"sync"
+
+	"github.com/grailbio/base/log"
+	"github.com/grailbio/base/retry"
+	"github.com/grailbio/base/sync/ctxsync"
+)
+
+// Policy implements the low level details of an admission control
+// policy. Users typically use a utility function such as admit.Do
+// or admit.Retry.
+type Policy interface {
+	// Acquire acquires a number of tokens from the admission controller.
+	// Returns on success, or if the context was canceled.
+	// Acquire can also return with an error if the number of requested tokens
+	// exceeds the upper limit of available tokens.
+	Acquire(ctx context.Context, need int) error
+
+	// Release a number of tokens to the admission controller,
+	// reporting whether the request was within the capacity limits.
+	Release(tokens int, ok bool)
+}
+
+// RetryPolicy combines an admission controller with a retry policy.
+type RetryPolicy interface {
+	Policy
+	retry.Policy
+}
+
+// ErrOverCapacity should be thrown by the "do" func passed to admit.Do or admit.Retry
+// for it to be considered an over capacity error.
+var ErrOverCapacity = errors.New("over capacity")
+
+// Do calls the provided function after being admitted by the admission controller.
+// If the function returns ErrOverCapacity, it is then reported as a capacity request
+// to the underlying policy.
+func Do(ctx context.Context, policy Policy, tokens int, do func() error) error {
+	if err := policy.Acquire(ctx, tokens); err != nil {
+		return err
+	}
+	var err error
+	defer func(err *error) {
+		policy.Release(tokens, *err != ErrOverCapacity)
+	}(&err)
+	err = do()
+	return err
+}
+
+// Retry calls the provided function with the combined retry and admission policies.
+func Retry(ctx context.Context, policy RetryPolicy, tokens int, do func() error) error {
+	var err error
+	for retries := 0; ; retries++ {
+		err = Do(ctx, policy, tokens, do)
+		// Retry as per retry policy if attempt failed due to over capacity.
+		if err != ErrOverCapacity {
+			break
+		}
+		if err = retry.Wait(ctx, policy, retries); err != nil {
+			break
+		}
+		log.Debug.Printf("admit.Retry: %v, retries=%d", err, retries)
+	}
+	return err
+}
+
+const defaultLimitChangeRate = 0.1
+
+// Adjust either increases or decreases the given limit by defaultChangeRate.
+func adjust(limit int, increase bool) int {
+	var change float32
+	if increase {
+		change = 1.0 + defaultLimitChangeRate
+	} else {
+		change = 1.0 - defaultLimitChangeRate
+	}
+	return int(float32(limit) * change)
+}
+
+type controller struct {
+	retry.Policy
+	max, used int
+	mu        sync.Mutex
+	cond      *ctxsync.Cond
+}
+
+func newController(limit int, retryPolicy retry.Policy) *controller {
+	c := &controller{Policy: retryPolicy, max: limit, used: 0}
+	c.cond = ctxsync.NewCond(&c.mu)
+	return c
+}
+
+// Controller returns a Policy which limits concurrency to given limit.
+// A controller is not fair: tokens are not granted in FIFO order;
+// rather, waiters are picked randomly to be granted new tokens.
+func Controller(limit int) Policy {
+	return ControllerWithRetry(limit, nil)
+}
+
+// ControllerWithRetry returns a RetryPolicy which limits concurrency to given limit.
+// A controller is not fair: tokens are not granted in FIFO order;
+// rather, waiters are picked randomly to be granted new tokens.
+func ControllerWithRetry(limit int, retryPolicy retry.Policy) RetryPolicy {
+	return newController(limit, retryPolicy)
+}
+
+// Acquire acquires a number of tokens from the admission controller.
+// Returns on success, or if the context was canceled.
+func (c *controller) Acquire(ctx context.Context, need int) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for {
+		// TODO(swami): should allow an increase only when the last release was ok
+		lim := adjust(c.max, true)
+		have := lim - c.used
+		if need <= have || (need > lim && c.used == 0) {
+			c.used += need
+			return nil
+		}
+		if err := c.cond.Wait(ctx); err != nil {
+			return err
+		}
+	}
+}
+
+// Release releases a number of tokens to the admission controller,
+// reporting whether the request was within the capacity limits.
+func (c *controller) Release(tokens int, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if ok {
+		if c.used > c.max {
+			c.max = c.used
+		}
+	} else {
+		c.max = adjust(c.max, false)
+	}
+	c.used -= tokens
+	c.cond.Broadcast()
+}
---
+++ admit/admit_test.go
@@ -0,0 +1,144 @@
+// Copyright 2018 GRAIL, Inc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package admit
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/grailbio/base/retry"
+	"github.com/grailbio/base/traverse"
+)
+
+func checkState(t *testing.T, c *controller, max, used int) {
+	t.Helper()
+	if c.used != used {
+		t.Errorf("c.used: got %d, want %d", c.used, used)
+	}
+	if c.max != max {
+		t.Errorf("c.max: got %d, want %d", c.max, max)
+	}
+}
+
+func TestController(t *testing.T) {
+	c := newController(10, nil)
+	// use up 5.
+	if err := c.Acquire(context.Background(), 5); err != nil {
+		t.Fatal(err)
+	}
+	checkState(t, c, 10, 5)
+	// can go upto 6.
+	if err := c.Acquire(context.Background(), 6); err != nil {
+		t.Fatal(err)
+	}
+	// release and report capacity error.
+	c.Release(5, false)
+	checkState(t, c, 9, 6)
+	ctx, _ := context.WithTimeout(context.Background(), time.Millisecond)
+	// 6 still in use and max should now be 9, so can't acquire 4.
+	if want, got := context.DeadlineExceeded, c.Acquire(ctx, 4); got != want {
+		t.Fatalf("got %v, want %v", got, want)
+	}
+	c.Release(6, true)
+	checkState(t, c, 9, 0)
+	// max is still 9, but since none are used, should accommodate larger request.
+	if err := c.Acquire(context.Background(), 12); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestControllerConcurrently(t *testing.T) {
+	const (
+		N = 100
+		T = 100
+	)
+	var pending int32
+	c := Controller(100)
+	var begin sync.WaitGroup
+	begin.Add(N)
+	err := traverse.Each(N, func(i int) error {
+		begin.Done()
+		n := rand.Intn(T/10) + 1
+		if err := c.Acquire(context.Background(), n); err != nil {
+			return err
+		}
+		if m := atomic.AddInt32(&pending, int32(n)); m > T {
+			return fmt.Errorf("too many tokens: %d > %d", m, T)
+		}
+		atomic.AddInt32(&pending, -int32(n))
+		c.Release(n, (i > 10 && i < 20) || (i > 70 && i < 80))
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDo(t *testing.T) {
+	someErr := errors.New("some other error")
+	c := newController(100, nil)
+	// Must satisfy even 150 tokens since none are used.
+	if err := Do(context.Background(), c, 150, func() error { return nil }); err != nil {
+		t.Fatal(err)
+	}
+	checkState(t, c, 150, 0)
+	// controller has 150 tokens, use 10 and report capacity error
+	if want, got := ErrOverCapacity, Do(context.Background(), c, 10, func() error { return ErrOverCapacity }); got != want {
+		t.Fatalf("got %v, want %v", got, want)
+	}
+	checkState(t, c, 135, 0)
+	// controller has 135 tokens, use up 35...
+	c.Acquire(context.Background(), 35)
+	checkState(t, c, 135, 35)
+	// can go upto 1.1*135 = 148, so should timeout for 114.
+	ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	if want, got := context.DeadlineExceeded, Do(ctx, c, 114, func() error { return someErr }); got != want {
+		t.Fatalf("got %v, want %v", got, want)
+	}
+	checkState(t, c, 135, 35)
+	// can go upto 1.1*135 = 148, so should timeout for 113.
+	if want, got := someErr, Do(context.Background(), c, 113, func() error { return someErr }); got != want {
+		t.Fatalf("got %v, want %v", got, want)
+	}
+	checkState(t, c, 148, 35)
+	// can go upto 1.1*148 = 162, so should go upto 127.
+	if err := Do(context.Background(), c, 127, func() error { return nil }); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRetry(t *testing.T) {
+	const (
+		N = 1000
+	)
+	c := ControllerWithRetry(200, retry.MaxTries(retry.Backoff(100*time.Millisecond, time.Minute, 1.5), 5))
+	var begin sync.WaitGroup
+	begin.Add(N)
+	err := traverse.Each(N, func(i int) error {
+		begin.Done()
+		begin.Wait()
+		randFunc := func() error {
+			if i%2 == 0 { // Every other request can potentially fail ...
+				time.Sleep(time.Millisecond * time.Duration(20+rand.Intn(50)))
+				if rand.Intn(100) < 5 { // 5% of the time.
+					return ErrOverCapacity
+				}
+			}
+			time.Sleep(time.Millisecond * time.Duration(5+rand.Intn(20)))
+			return nil
+		}
+		n := rand.Intn(20) + 1
+		return Retry(context.Background(), c, n, randFunc)
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
