d6120c2e64e4dc282a177299e65b0dfd4dcab34a
marius a. eriksen <marius@grailbio.com>
2018-11-26 15:44:34 -0800

    fix bad sync

---
+++ cmd/grail-file/grail_internal.go
@@ -0,0 +1,5 @@
+package main
+
+import (
+	_ "grail.com/cloud/grailfile"
+)
---
+++ sync/once/once.go
@@ -0,0 +1,54 @@
+// Copyright 2018 GRAIL, Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Package once contains utilities for managing actions that
+// must be performed exactly once.
+package once
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+// Task manages a computation that must be run at most once.
+// It's similar to sync.Once, except it also handles and returns errors.
+type Task struct {
+	mu   sync.Mutex
+	done uint32
+	err  error
+}
+
+// Do run the function do at most once. Successive invocations of Do
+// guarantee exactly one invocation of the function do. Do returns
+// the error of do's invocation.
+func (o *Task) Do(do func() error) error {
+	if atomic.LoadUint32(&o.done) == 1 {
+		return o.err
+	}
+	o.mu.Lock()
+	defer o.mu.Unlock()
+	if atomic.LoadUint32(&o.done) == 0 {
+		o.err = do()
+		atomic.StoreUint32(&o.done, 1)
+	}
+	return o.err
+}
+
+// Map coordinates actions that must happen exactly once, keyed
+// by user-defined keys.
+type Map sync.Map
+
+// Perform the provided action named by a key. Do invokes the action
+// exactly once for each key, and returns any errors produced by the
+// provided action.
+func (m *Map) Do(key interface{}, do func() error) error {
+	taskv, _ := (*sync.Map)(m).LoadOrStore(key, new(Task))
+	task := taskv.(*Task)
+	return task.Do(do)
+}
+
+// Forget forgets past computations associated with the provided key.
+func (m *Map) Forget(key interface{}) {
+	(*sync.Map)(m).Delete(key)
+}
