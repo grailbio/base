a104d0d0bb09eeca435e19e8feaf1de5de9b6dbd
Christopher Chang <cchang@grailbio.com>
2019-01-11 17:28:26 -0800

    bio/biosimd: add non-amd64 fallbacks
    Summary:
    This makes bio/biosimd usable by code that can't assume an amd64 build
    target.  Analogous to D23630.
    
    (Note that gazelle chooses an overly verbose representation of the
    library dependencies.)
    
    Test Plan: Existing tests pass with build tags flipped.
    
    Reviewers: sbagaria, ysaito
    
    Reviewed By: sbagaria
    
    Subscribers: cchang
    
    Differential Revision: https://phabricator.grailbio.com/D23860
    
    fbshipit-source-id: 29311ea

--- simd/multibyte_generic.go
+++ simd/multibyte_generic.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Apache-2.0
 // license that can be found in the LICENSE file.
 
-// +build !amd64 appengine
+// +build !amd64,!appengine
 
 package simd
 
