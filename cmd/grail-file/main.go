d4f4346249391d306eb2c86644257baee7974cd2
Yaz Saito <ysaito@grailbio.com>
2019-03-18 10:26:50 -0700

    grail-file: Fix the open-source binary
    Summary: It wasn't registering the S3 factor.
    
    Reviewers: marius, sbagaria
    
    Reviewed By: marius
    
    Differential Revision: https://phabricator.grailbio.com/D26991
    
    fbshipit-source-id: ed7e697

--- cmd/grail-file/main.go
+++ cmd/grail-file/main.go
@@ -4,12 +4,18 @@ import (
 	"context"
 	"os"
 
-	"github.com/grailbio/base/log"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/grailbio/base/cmd/grail-file/cmd"
+	"github.com/grailbio/base/file"
+	"github.com/grailbio/base/file/s3file"
+	"github.com/grailbio/base/log"
 )
 
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+	file.RegisterImplementation("s3", func() file.Implementation {
+		return s3file.NewImplementation(s3file.NewDefaultProvider(session.Options{}), s3file.Options{})
+	})
 	err := cmd.Run(context.Background(), os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
