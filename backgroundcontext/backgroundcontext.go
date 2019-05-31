22a2d34f23b93a1ce63b921ff4d5accc7a2ca2ee
marius a. eriksen <marius@grailbio.com>
2019-05-31 10:40:36 -0700

    base/backgroundcontext: add Get method to get a background context; use in Bigslice
    Summary:
    This change adds backgroundcontext.Get(). This can be used to in
    place of "context.Background()" in libraries that need background
    contexts.
    
    The idea is that libraries may or may not be used in the context of a
    v23 application, and thus need a way to get a generic background
    context that can serve either kind of application.
    
    Reviewers: ysaito, jcharumilind, pgopal
    
    Reviewed By: ysaito
    
    Differential Revision: https://phabricator.grailbio.com/D31170
    
    fbshipit-source-id: b33ddee

--- backgroundcontext/backgroundcontext.go
+++ backgroundcontext/backgroundcontext.go
@@ -2,13 +2,14 @@
 // Use of this source code is governed by the Apache-2.0
 // license that can be found in the LICENSE file.
 
-// Package backgroundcontext manages the singleton v23 context.  This package is
-// not for general use. It it only for packages that (1) need to access a
-// background context, and (2) are used both as a binary and as a shared library
-// (e.g., in R).
+// Package backgroundcontext manages the singleton v23 context.  This
+// package is not for general use. It it only for packages that (1)
+// need to access a background context, and (2) are used both as a
+// binary and as a shared library (e.g., in R).
 package backgroundcontext
 
 import (
+	"context"
 	"sync/atomic"
 	"unsafe"
 
@@ -34,4 +35,44 @@ func T() *v23context.T {
 		panic("backgroundcontext.Set not yet called")
 	}
 	return (*v23context.T)(p)
+}
+
+// Get returns a background context: if a v23 context has been set, it is returned;
+// otherwise the standard Go background context is returned.
+func Get() context.Context {
+	if p := (*v23context.T)(atomic.LoadPointer(&ptr)); p != nil {
+		return p
+	}
+	return context.Background()
+}
+
+type wrapped struct {
+	context.Context
+	v23ctx *v23context.T
+}
+
+func (w *wrapped) Value(key interface{}) interface{} {
+	val := w.Context.Value(key)
+	if val != nil {
+		return val
+	}
+	return w.v23ctx.Value(key)
+}
+
+// Wrap wraps the provided context, composing it with the defined
+// background context, if any. This allows contexts to be wrapped
+// so that v23 stubs can get the background context's RPC client.
+//
+// Cancelations are not forwarded: it is assumed that the set background
+// context is never canceled.
+//
+// BUG: this is very complicated; but it seems required to make
+// vanadium play nicely with contexts defined outside of its
+// universe.
+func Wrap(ctx context.Context) context.Context {
+	v23ctx := (*v23context.T)(atomic.LoadPointer(&ptr))
+	if v23ctx == nil {
+		return ctx
+	}
+	return &wrapped{ctx, v23ctx}
 }
