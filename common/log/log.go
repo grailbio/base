9643ee2529eb764b54f27206873d5950636abfae
Noah Tseng <ntseng@grailbio.com>
2021-03-23 09:26:36 -1000

    common/log: log.Error*() should return an error
    Summary: Return an `error` with `log.Error*()` for convenience. This change should be backwards compatible and not cause any lint issues. Users can optionally use or ignore the returned `error`.
    
    Test Plan: .
    
    Reviewers: O63 logger, fdegiuli
    
    Reviewed By: O63 logger, fdegiuli
    
    Subscribers: fdegiuli, smahadevan
    
    Differential Revision: https://phabricator.grailbio.com/D56516
    
    fbshipit-source-id: 8015af8

--- common/log/log.go
+++ common/log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"context"
+	"errors"
 	"fmt"
 )
 
@@ -94,27 +95,28 @@ func WarnNoCtx(msg string, keysAndValues ...interface{}) {
 // Error logs a message, the key-value pairs defined in contextFields from ctx, and variadic key-value pairs.
 // If ctx is nil, all fields from contextFields will be omitted.
 // If ctx does not contain a key in contextFields, that field will be omitted.
-func Error(ctx context.Context, msg string, keysAndValues ...interface{}) {
-	Errorv(ctx, 1, msg, keysAndValues...)
+func Error(ctx context.Context, msg string, keysAndValues ...interface{}) error {
+	return Errorv(ctx, 1, msg, keysAndValues...)
 }
 
 // Errorf uses fmt.Sprintf to log a templated message and the key-value pairs defined in contextFields from ctx.
 // If ctx is nil, all fields from contextFields will be omitted.
 // If ctx does not contain a key in contextFields, that field will be omitted.
-func Errorf(ctx context.Context, fs string, args ...interface{}) {
-	Errorv(ctx, 1, fmt.Sprintf(fs, args...))
+func Errorf(ctx context.Context, fs string, args ...interface{}) error {
+	return Errorv(ctx, 1, fmt.Sprintf(fs, args...))
 }
 
 // Errorv logs a message, the key-value pairs defined in contextFields from ctx, and variadic key-value pairs.
 // Caller is skipped by skip.
 // If ctx is nil, all fields from contextFields will be omitted.
 // If ctx does not contain a key in contextFields, that field will be omitted.
-func Errorv(ctx context.Context, skip int, msg string, keysAndValues ...interface{}) {
+func Errorv(ctx context.Context, skip int, msg string, keysAndValues ...interface{}) error {
 	logger.Errorv(ctx, skip+1, msg, keysAndValues...)
+	return errors.New(msg)
 }
 
 // ErrorNoCtx logs a message and variadic key-value pairs.
-func ErrorNoCtx(msg string, keysAndValues ...interface{}) {
+func ErrorNoCtx(msg string, keysAndValues ...interface{}) error {
 	// context.Background() is a singleton and gets initialized once
-	Errorv(context.Background(), 1, msg, keysAndValues...)
+	return Errorv(context.Background(), 1, msg, keysAndValues...)
 }
