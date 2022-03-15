3cd69d0e065528b35d0adc2162459b4927b33977
Josh Newman <joshnewman@grailbio.com>
2022-03-15 18:39:38 +0000

    bigslice: add logging for "extreme" argument representation truncation
    Test Plan:
    New limitbuf unit test.
    
    Run a bigslice application that triggers this and look for the log message.
    
    Reviewers: jcharumilind
    
    Reviewed By: jcharumilind
    
    Subscribers: smahadevan
    
    Differential Revision: https://phabricator.grailbio.com/D71459
    
    fbshipit-source-id: 906ef12

--- limitbuf/limitbuf_test.go
+++ limitbuf/limitbuf_test.go
@@ -1,9 +1,11 @@
 package limitbuf_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/grailbio/base/limitbuf"
+	"github.com/grailbio/base/log"
 	"github.com/grailbio/testutil/expect"
 )
 
@@ -12,6 +14,30 @@ func TestLogger(t *testing.T) {
 	l.Write([]byte("blah"))
 	expect.EQ(t, l.String(), "blah")
 	l.Write([]byte("abcdefgh"))
-	expect.EQ(t, l.String(), "blahabcdef(truncated)")
-	expect.EQ(t, l.String(), "blahabcdef(truncated)")
+	expect.EQ(t, l.String(), "blahabcdef(truncated 2 bytes)")
+	expect.EQ(t, l.String(), "blahabcdef(truncated 2 bytes)")
+}
+
+func TestLoggerExtremeTruncation(t *testing.T) {
+	oldOutputter := log.GetOutputter()
+	t.Cleanup(func() { log.SetOutputter(oldOutputter) })
+	var outputter testOutputter
+	log.SetOutputter(&outputter)
+
+	logger := limitbuf.NewLogger(2, limitbuf.LogIfTruncatingMaxMultiple(3))
+	_, err := logger.Write([]byte("abcdefg"))
+	expect.NoError(t, err)
+
+	expect.EQ(t, logger.String(), "ab(truncated 5 bytes)")
+	expect.HasSubstr(t, outputter.String(), "extreme truncation")
+}
+
+type testOutputter struct{ bytes.Buffer }
+
+func (o *testOutputter) Level() log.Level {
+	return log.Error
+}
+func (o *testOutputter) Output(_ int, _ log.Level, s string) error {
+	_, err := o.Buffer.WriteString(s)
+	return err
 }
