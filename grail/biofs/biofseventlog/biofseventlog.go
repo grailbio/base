bf00428e70edad9a2e528102e908aae1adb18444
Josh N <joshnewman@grailbio.com>
2022-06-16 16:58:30 +0000

    biofs: configure cloudwatch eventer
    Approved-by: Jaran Charumilind <jcharumilind@grailbio.com>
    
    GitLab URL: https://gitlab.com/grailbio/grail/-/merge_requests/1175
    
    fbshipit-source-id: 7b394bc

--- grail/biofs/biofseventlog/biofseventlog.go
+++ grail/biofs/biofseventlog/biofseventlog.go
@@ -4,8 +4,13 @@
 package biofseventlog
 
 import (
+	"strconv"
+	"sync"
+	"time"
+
 	"github.com/grailbio/base/config"
 	"github.com/grailbio/base/eventlog"
+	"github.com/grailbio/base/log"
 	"github.com/grailbio/base/must"
 )
 
@@ -19,5 +24,43 @@ func init() {
 func UsedFeature(featureName string) {
 	var eventer eventlog.Eventer
 	must.Nil(config.Instance(configName, &eventer))
-	eventer.Event("usedFeature", "name", featureName)
+	eventer.Event("usedFeature",
+		"name", featureName,
+		"buildTime", getCoarseBuildTimestamp())
+}
+
+// CoarseNow returns times with precision truncated by CoarseTime.
+func CoarseNow() time.Time { return CoarseTime(time.Now()) }
+
+// CoarseTime truncates t's precision to a nearby week. It's used to improve event log anonymity.
+func CoarseTime(t time.Time) time.Time {
+	weekMillis := 7 * 24 * time.Hour.Milliseconds()
+	now := t.UnixMilli()
+	now /= weekMillis
+	now *= weekMillis
+	return time.UnixMilli(now)
+}
+
+var (
+	buildTimestamp       string
+	coarseBuildTimestamp = "unknown"
+	buildTimestampOnce   sync.Once
+)
+
+// getCoarseBuildTimestamp returns the (probably bazel-injected) build timestamp
+// with precision truncated to CoarseTime, or a message if data is unavailable.
+func getCoarseBuildTimestamp() string {
+	buildTimestampOnce.Do(func() {
+		if buildTimestamp == "" {
+			return
+		}
+		buildSecs, err := strconv.ParseInt(buildTimestamp, 10, 64)
+		if err != nil {
+			log.Error.Printf("biofseventlog: error parsing build timestamp: %v", err)
+			return
+		}
+		coarseBuildTime := CoarseTime(time.Unix(buildSecs, 0))
+		coarseBuildTimestamp = coarseBuildTime.Format("20060102")
+	})
+	return coarseBuildTimestamp
 }
