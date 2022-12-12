5aa29f99299894f1bed81fddafb5de7b70c47cbf
Jaran Charumilind <jcharumilind@grailbio.com>
2022-12-12 19:55:10 +0000

    gfilefs: Make optional S3 tests run in addition to local tests (grailbio/grail!9248)
    Approved-by: Josh Newman <joshnewman@grailbio.com>
    
    GitLab URL: https://gitlab.com/grailbio/grail/-/merge_requests/9248
    
    fbshipit-source-id: b1d7fb1

--- file/gfilefs/write_test.go
+++ file/gfilefs/write_test.go
@@ -37,15 +37,14 @@ func init() {
 	})
 }
 
-// rootFlag sets a the root directory to use for test files.  Normally, the
-// root is a temporary local directory.  It can be useful to run this test as
-// an integration test with base/file backed by S3.  Set root to be an empty
-// path in S3 to which you have write access to verify that writes behave well
-// with S3 objects.
-var rootFlag = flag.String(
-	"root",
+// s3RootFlag sets an S3 root directory to use for test files.  When set to a
+// non-empty S3 path, e.g. "s3://some-bucket/some-writable/prefix", tests will
+// run with a mount point with this root.  These tests will run in addition to
+// the normal local root testing.
+var s3RootFlag = flag.String(
+	"s3-root",
 	"",
-	"the (empty) root directory to use for test files",
+	"optional S3 root directory to use for testing, in addition to the local root",
 )
 
 // TestCreateEmpty verifies that we can create an new empty file using various
@@ -96,24 +95,22 @@ func TestCreateEmpty(t *testing.T) {
 		return strings.Join(parts, "_")
 	}
 	for _, flag := range combos(flagElements) {
-		t.Run(name(flag), func(t *testing.T) {
-			var (
-				m, cleanUp = makeTestMount(t)
-				path       = filepath.Join(m.dir, "test")
-			)
-			defer cleanUp()
-			flag |= os.O_CREATE
-			f, err := os.OpenFile(path, flag, 0666)
-			require.NoError(t, err, "creating file")
-			require.NoError(t, f.Close(), "closing file")
+		withTestMounts(t, func(m testMount) {
+			t.Run(name(flag), func(t *testing.T) {
+				path := filepath.Join(m.mountPoint, "test")
+				flag |= os.O_CREATE
+				f, err := os.OpenFile(path, flag, 0666)
+				require.NoError(t, err, "creating file")
+				require.NoError(t, f.Close(), "closing file")
 
-			info, err := os.Stat(path)
-			require.NoError(t, err, "stat of file")
-			assert.Equal(t, int64(0), info.Size(), "file should have zero size")
+				info, err := os.Stat(path)
+				require.NoError(t, err, "stat of file")
+				assert.Equal(t, int64(0), info.Size(), "file should have zero size")
 
-			bs, err := ioutil.ReadFile(path)
-			require.NoError(t, err, "reading file")
-			assert.Empty(t, bs, "file should be empty")
+				bs, err := ioutil.ReadFile(path)
+				require.NoError(t, err, "reading file")
+				assert.Empty(t, bs, "file should be empty")
+			})
 		})
 	}
 }
@@ -121,34 +118,34 @@ func TestCreateEmpty(t *testing.T) {
 // TestCreate verifies that we can create a new file, write content to it, and
 // read the same content back.
 func TestCreate(t *testing.T) {
-	var (
-		r          = rand.New(rand.NewSource(0))
-		m, cleanUp = makeTestMount(t)
-		path       = filepath.Join(m.dir, "test")
-		rootPath   = file.Join(m.root, "test")
-	)
-	defer cleanUp()
-	assertRoundTrip(t, path, rootPath, r, 10*(1<<20))
-	assertRoundTrip(t, path, rootPath, r, 10*(1<<16))
+	withTestMounts(t, func(m testMount) {
+		var (
+			r        = rand.New(rand.NewSource(0))
+			path     = filepath.Join(m.mountPoint, "test")
+			rootPath = file.Join(m.root, "test")
+		)
+		assertRoundTrip(t, path, rootPath, r, 10*(1<<20))
+		assertRoundTrip(t, path, rootPath, r, 10*(1<<16))
+	})
 }
 
 // TestOverwrite verifies that we can overwrite the same file repeatedly, and
 // that the updated content is correct.
 func TestOverwrite(t *testing.T) {
-	const NumOverwrites = 20
-	var (
-		r          = rand.New(rand.NewSource(0))
-		m, cleanUp = makeTestMount(t)
-		path       = filepath.Join(m.dir, "test")
-		rootPath   = file.Join(m.root, "test")
-	)
-	defer cleanUp()
-	for i := 0; i < NumOverwrites+1; i++ {
-		// Each iteration uses a random size between 5 and 10 MiB.
-		n := 5 + r.Intn(10)
-		n *= 1 << 20
-		assertRoundTrip(t, path, rootPath, r, n)
-	}
+	withTestMounts(t, func(m testMount) {
+		const NumOverwrites = 20
+		var (
+			r        = rand.New(rand.NewSource(0))
+			path     = filepath.Join(m.mountPoint, "test")
+			rootPath = file.Join(m.root, "test")
+		)
+		for i := 0; i < NumOverwrites+1; i++ {
+			// Each iteration uses a random size between 5 and 10 MiB.
+			n := 5 + r.Intn(10)
+			n *= 1 << 20
+			assertRoundTrip(t, path, rootPath, r, n)
+		}
+	})
 }
 
 // TestTruncFlag verifies that opening with O_TRUNC truncates the file.
@@ -162,32 +159,29 @@ func TestTruncFlag(t *testing.T) {
 }
 
 func testTruncFlag(t *testing.T, flag int) {
-	var (
-		m, cleanUp = makeTestMount(t)
-		path       = filepath.Join(m.dir, "test")
-	)
-	defer cleanUp()
+	withTestMounts(t, func(m testMount) {
+		path := filepath.Join(m.mountPoint, "test")
+		// Write the file we will truncate to test.
+		err := ioutil.WriteFile(path, []byte{0, 1, 2}, 0644)
+		require.NoError(t, err, "writing file")
 
-	// Write the file we will truncate to test.
-	err := ioutil.WriteFile(path, []byte{0, 1, 2}, 0644)
-	require.NoError(t, err, "writing file")
-
-	f, err := os.OpenFile(path, flag|os.O_TRUNC, 0666)
-	require.NoError(t, err, "opening for truncation")
-	func() {
-		defer func() {
-			require.NoError(t, f.Close())
+		f, err := os.OpenFile(path, flag|os.O_TRUNC, 0666)
+		require.NoError(t, err, "opening for truncation")
+		func() {
+			defer func() {
+				require.NoError(t, f.Close())
+			}()
+			var info gofs.FileInfo
+			info, err = f.Stat()
+			require.NoError(t, err, "getting file stats")
+			assert.Equal(t, int64(0), info.Size(), "truncated file should be zero bytes")
 		}()
-		var info gofs.FileInfo
-		info, err = f.Stat()
-		require.NoError(t, err, "getting file stats")
-		assert.Equal(t, int64(0), info.Size(), "truncated file should be zero bytes")
-	}()
 
-	// Verify that reading the truncated file yields zero bytes.
-	bsRead, err := ioutil.ReadFile(path)
-	require.NoError(t, err, "reading truncated file")
-	assert.Empty(t, bsRead, "reading truncated file should yield no data")
+		// Verify that reading the truncated file yields zero bytes.
+		bsRead, err := ioutil.ReadFile(path)
+		require.NoError(t, err, "reading truncated file")
+		assert.Empty(t, bsRead, "reading truncated file should yield no data")
+	})
 }
 
 // TestTruncateZero verifies that truncation to zero works.
@@ -201,184 +195,181 @@ func TestTruncateZero(t *testing.T) {
 }
 
 func testTruncateZero(t *testing.T, flag int) {
-	var (
-		m, cleanUp = makeTestMount(t)
-		path       = filepath.Join(m.dir, "test")
-	)
-	defer cleanUp()
+	withTestMounts(t, func(m testMount) {
+		path := filepath.Join(m.mountPoint, "test")
+		// Write the file we will truncate to test.
+		err := ioutil.WriteFile(path, []byte{0, 1, 2}, 0644)
+		require.NoError(t, err, "writing file")
 
-	// Write the file we will truncate to test.
-	err := ioutil.WriteFile(path, []byte{0, 1, 2}, 0644)
-	require.NoError(t, err, "writing file")
+		f, err := os.OpenFile(path, os.O_WRONLY, 0666)
+		require.NoError(t, err, "opening for truncation")
 
-	f, err := os.OpenFile(path, os.O_WRONLY, 0666)
-	require.NoError(t, err, "opening for truncation")
+		func() {
+			defer func() {
+				require.NoError(t, f.Close(), "closing")
+			}()
+			// Sanity check that the initial file handle is the correct size.
+			var info gofs.FileInfo
+			info, err = f.Stat()
+			require.NoError(t, err, "getting file stats")
+			assert.Equal(t, int64(3), info.Size(), "file to truncate should be three bytes")
 
-	func() {
-		defer func() {
-			require.NoError(t, f.Close(), "closing")
+			require.NoError(t, f.Truncate(0), "truncating")
+
+			// Verify that the file handle is actually truncated.
+			info, err = f.Stat()
+			require.NoError(t, err, "getting file stats")
+			assert.Equal(t, int64(0), info.Size(), "truncated file should be zero bytes")
 		}()
-		// Sanity check that the initial file handle is the correct size.
-		var info gofs.FileInfo
-		info, err = f.Stat()
-		require.NoError(t, err, "getting file stats")
-		assert.Equal(t, int64(3), info.Size(), "file to truncate should be three bytes")
 
-		require.NoError(t, f.Truncate(0), "truncating")
-
-		// Verify that the file handle is actually truncated.
-		info, err = f.Stat()
+		// Verify that an independent stat shows zero size.
+		info, err := os.Stat(path)
 		require.NoError(t, err, "getting file stats")
 		assert.Equal(t, int64(0), info.Size(), "truncated file should be zero bytes")
-	}()
 
-	// Verify that an independent stat shows zero size.
-	info, err := os.Stat(path)
-	require.NoError(t, err, "getting file stats")
-	assert.Equal(t, int64(0), info.Size(), "truncated file should be zero bytes")
-
-	// Verify that reading the truncated file yields zero bytes.
-	bsRead, err := ioutil.ReadFile(path)
-	require.NoError(t, err, "reading truncated file")
-	assert.Empty(t, bsRead, "reading truncated file should yield no data")
+		// Verify that reading the truncated file yields zero bytes.
+		bsRead, err := ioutil.ReadFile(path)
+		require.NoError(t, err, "reading truncated file")
+		assert.Empty(t, bsRead, "reading truncated file should yield no data")
+	})
 }
 
 // TestRemove verifies that we can remove a file.
 func TestRemove(t *testing.T) {
-	var (
-		r          = rand.New(rand.NewSource(0))
-		m, cleanUp = makeTestMount(t)
-		path       = filepath.Join(m.dir, "test")
-		rootPath   = file.Join(m.root, "test")
-	)
-	defer cleanUp()
-	bs := make([]byte, 1*(1<<20))
-	_, err := r.Read(bs)
-	require.NoError(t, err, "making random data")
-	err = ioutil.WriteFile(path, bs, 0644)
-	require.NoError(t, err, "writing file")
-	err = os.Remove(path)
-	require.NoError(t, err, "removing file")
-	_, err = os.Stat(path)
-	require.True(t, os.IsNotExist(err), "file was not removed")
-	_, err = os.Stat(rootPath)
-	require.True(t, os.IsNotExist(err), "file was not removed in root")
+	withTestMounts(t, func(m testMount) {
+		var (
+			r        = rand.New(rand.NewSource(0))
+			path     = filepath.Join(m.mountPoint, "test")
+			rootPath = file.Join(m.root, "test")
+		)
+		bs := make([]byte, 1*(1<<20))
+		_, err := r.Read(bs)
+		require.NoError(t, err, "making random data")
+		err = ioutil.WriteFile(path, bs, 0644)
+		require.NoError(t, err, "writing file")
+		err = os.Remove(path)
+		require.NoError(t, err, "removing file")
+		_, err = os.Stat(path)
+		require.True(t, os.IsNotExist(err), "file was not removed")
+		_, err = os.Stat(rootPath)
+		require.True(t, os.IsNotExist(err), "file was not removed in root")
+	})
 }
 
 // TestDirListing verifies that the directory listing of a file is updated when
 // the file is modified.
 func TestDirListing(t *testing.T) {
-	var (
-		m, cleanUp = makeTestMount(t)
-		path       = file.Join(m.dir, "test")
-	)
-	defer cleanUp()
+	withTestMounts(t, func(m testMount) {
+		path := file.Join(m.mountPoint, "test")
+		// assertSize asserts that the listed FileInfo of the file at path reports
+		// the given size.
+		assertSize := func(size int64) {
+			infos, err := ioutil.ReadDir(m.mountPoint)
+			require.NoError(t, err, "listing directory")
+			require.Equal(t, 1, len(infos), "should only be one file in directory")
+			assert.Equal(t, size, infos[0].Size(), "file should be 3 bytes")
+		}
 
-	// assertSize asserts that the listed FileInfo of the file at path reports
-	// the given size.
-	assertSize := func(size int64) {
-		infos, err := ioutil.ReadDir(m.dir)
-		require.NoError(t, err, "listing directory")
-		require.Equal(t, 1, len(infos), "should only be one file in directory")
-		assert.Equal(t, size, infos[0].Size(), "file should be 3 bytes")
-	}
+		// Write a 3-byte file, and verify that its listing has the correct size.
+		require.NoError(t, ioutil.WriteFile(path, make([]byte, 3), 0644), "writing file")
+		assertSize(3)
 
-	// Write a 3-byte file, and verify that its listing has the correct size.
-	require.NoError(t, ioutil.WriteFile(path, make([]byte, 3), 0644), "writing file")
-	assertSize(3)
+		// Overwrite it to be 1 byte, and verify that the listing is updated.
+		require.NoError(t, ioutil.WriteFile(path, make([]byte, 1), 0644), "overwriting file")
+		assertSize(1)
 
-	// Overwrite it to be 1 byte, and verify that the listing is updated.
-	require.NoError(t, ioutil.WriteFile(path, make([]byte, 1), 0644), "overwriting file")
-	assertSize(1)
-
-	// Append 3 bytes, and verify that the listing is updated.
-	f, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, 0644)
-	require.NoError(t, err, "opening file for append")
-	_, err = f.Write(make([]byte, 3))
-	require.NoError(t, err, "appending to file")
-	require.NoError(t, f.Close(), "closing file")
-	assertSize(4)
+		// Append 3 bytes, and verify that the listing is updated.
+		f, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, 0644)
+		require.NoError(t, err, "opening file for append")
+		_, err = f.Write(make([]byte, 3))
+		require.NoError(t, err, "appending to file")
+		require.NoError(t, f.Close(), "closing file")
+		assertSize(4)
+	})
 }
 
 // TestMkdir verifies that we can make a directory.
 func TestMkdir(t *testing.T) {
-	var (
-		r          = rand.New(rand.NewSource(0))
-		m, cleanUp = makeTestMount(t)
-		path       = filepath.Join(m.dir, "test-dir")
-	)
-	defer cleanUp()
-	err := os.Mkdir(path, 0775)
-	require.NoError(t, err, "making directory")
+	withTestMounts(t, func(m testMount) {
+		var (
+			r    = rand.New(rand.NewSource(0))
+			path = filepath.Join(m.mountPoint, "test-dir")
+		)
+		err := os.Mkdir(path, 0775)
+		require.NoError(t, err, "making directory")
 
-	filePath := filepath.Join(path, "test")
-	rootFilePath := file.Join(m.root, "test-dir", "test")
-	assertRoundTrip(t, filePath, rootFilePath, r, 0)
+		filePath := filepath.Join(path, "test")
+		rootFilePath := file.Join(m.root, "test-dir", "test")
+		assertRoundTrip(t, filePath, rootFilePath, r, 0)
 
-	info, err := os.Stat(path)
-	require.NoError(t, err, "getting file info of created directory")
-	require.True(t, info.IsDir(), "created directory is not a directory")
+		info, err := os.Stat(path)
+		require.NoError(t, err, "getting file info of created directory")
+		require.True(t, info.IsDir(), "created directory is not a directory")
+	})
+}
+
+func withTestMounts(t *testing.T, f func(m testMount)) {
+	type makeRootFunc func(*testing.T) (string, func())
+	makeRoots := map[string]makeRootFunc{
+		"local": func(t *testing.T) (string, func()) {
+			return testutil.TempDir(t, "", "gfilefs-mnt")
+		},
+	}
+	if *s3RootFlag != "" {
+		makeRoots["s3"] = func(t *testing.T) (string, func()) {
+			ctx := context.Background()
+			lister := file.List(ctx, *s3RootFlag, true)
+			exists := lister.Scan()
+			if exists {
+				t.Logf("path exists: %s", lister.Path())
+			}
+			require.NoErrorf(t, lister.Err(), "listing %s", *s3RootFlag)
+			require.False(t, exists)
+			return *s3RootFlag, func() {
+				err := forEachFile(ctx, *s3RootFlag, func(path string) error {
+					return file.Remove(ctx, path)
+				})
+				require.NoError(t, err, "cleaning up test root")
+			}
+		}
+	}
+	for name, makeRoot := range makeRoots {
+		t.Run(name, func(t *testing.T) {
+			root, rootCleanUp := makeRoot(t)
+			defer rootCleanUp()
+			mountPoint, mountPointCleanUp := testutil.TempDir(t, "", "gfilefs-mnt")
+			defer mountPointCleanUp()
+			server, err := fs.Mount(
+				mountPoint,
+				fsnodefuse.NewRoot(gfilefs.New(root, "root")),
+				&fs.Options{
+					MountOptions: fuse.MountOptions{
+						FsName:        "test",
+						DisableXAttrs: true,
+						Debug:         true,
+						MaxBackground: 1024,
+					},
+				},
+			)
+			require.NoError(t, err, "mounting %q", mountPoint)
+			defer func() {
+				log.Printf("unmounting %q", mountPoint)
+				assert.NoError(t, server.Unmount(),
+					"unmount of FUSE mounted at %q failed; may need manual cleanup",
+					mountPoint,
+				)
+				log.Printf("unmounted %q", mountPoint)
+			}()
+			f(testMount{root: root, mountPoint: mountPoint})
+		})
+	}
 }
 
 type testMount struct {
+	// root is the root path that is mounted at dir.
 	root string
-	dir  string
-}
-
-func makeTestMount(t *testing.T) (_ testMount, cleanUp func()) {
-	var (
-		root        string
-		rootCleanUp func()
-	)
-	if *rootFlag != "" {
-		root = *rootFlag
-		ctx := context.Background()
-		lister := file.List(ctx, root, true)
-		exists := lister.Scan()
-		if exists {
-			t.Logf("path exists: %s", lister.Path())
-		}
-		require.NoErrorf(t, lister.Err(), "listing %s", root)
-		require.False(t, exists)
-
-		rootCleanUp = func() {
-			err := forEachFile(ctx, root, func(path string) error {
-				return file.Remove(ctx, path)
-			})
-			require.NoError(t, err, "cleaning up test root")
-		}
-	} else {
-		root, rootCleanUp = testutil.TempDir(t, "", "gfilefs-root")
-	}
-	var (
-		dir, dirCleanUp = testutil.TempDir(t, "", "gfilefs-mnt")
-		server, err     = fs.Mount(
-			dir,
-			fsnodefuse.NewRoot(gfilefs.New(root, "root")),
-			&fs.Options{
-				MountOptions: fuse.MountOptions{
-					FsName:        "test",
-					DisableXAttrs: true,
-					Debug:         true,
-					MaxBackground: 1024,
-				},
-			},
-		)
-	)
-	require.NoError(t, err, "mounting %q", dir)
-	return testMount{root: root, dir: dir},
-		func() {
-			// log.Printf("sleeping before unmounting")
-			// time.Sleep(1 * time.Second)
-			log.Printf("unmounting %q", dir)
-			assert.NoError(t, server.Unmount(),
-				"unmount of FUSE mounted at %q failed; may need manual cleanup",
-				dir,
-			)
-			log.Printf("unmounted %q", dir)
-			dirCleanUp()
-			rootCleanUp()
-		}
+	// mountPoint is the FUSE mount point.
+	mountPoint string
 }
 
 // forEachFile runs the callback for every file under the directory in
